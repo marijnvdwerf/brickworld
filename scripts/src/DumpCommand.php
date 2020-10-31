e6421c7e147dc4ebf376d92506c2afbff5df3a83
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2020-10-31 14:32:07 +0100

    Read tree and dump files

--- scripts/src/DumpCommand.php
+++ scripts/src/DumpCommand.php
@@ -2,8 +2,11 @@
 
 namespace LegoMedia\LegoLand;
 
+use PhpBinaryReader\BinaryReader;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class DumpCommand extends Command
 {
@@ -11,6 +14,63 @@ class DumpCommand extends Command
     {
         $this
             ->setName('dump-res')
-            ->addArgument('res', InputArgument::REQUIRED, '.res file');
+            ->addArgument('res', InputArgument::REQUIRED, '.res file')
+            ->addArgument('dest', InputArgument::REQUIRED);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $file = fopen($input->getArgument('res'), 'rb');
+
+        $basedir = $input->getArgument('dest');
+
+        $br = new BinaryReader($file);
+        $dataStart = $br->readUInt32();
+
+        $br->setPosition($dataStart);
+        $entries = $this->readEntry($br, $dataStart);
+
+        foreach ($entries as $e) {
+            $this->saveEntry($e, $basedir, $file);
+        }
+
+        return 0;
+    }
+
+    protected function readEntry(BinaryReader $br, $startOffset)
+    {
+        $e = ResEntry::read($br);
+
+        if ($e->a != 0xFFFFFFFF) {
+            $br->setPosition($startOffset + $e->a);
+            $e->children = $this->readEntry($br, $startOffset);
+        }
+
+        $siblings = [];
+        if ($e->b != 0xFFFFFFFF) {
+            $br->setPosition($startOffset + $e->b);
+            $siblings = $this->readEntry($br, $startOffset);
+        }
+
+        return array_merge([$e], $siblings);
+    }
+
+    private function saveEntry(ResEntry $entry, $basedir, $fh)
+    {
+        $path = $basedir . '/' . $entry->name;
+        if (!file_exists(dirname($path))) {
+            mkdir(dirname($path), 0777, true);
+        }
+
+        if (!$entry->isFolder) {
+            $dest = fopen($path, 'w+');
+            fseek($fh, $entry->e);
+            stream_copy_to_stream($fh, $dest, $entry->d);
+        }
+
+
+        foreach ($entry->children as $child) {
+            $this->saveEntry($child, $path, $fh);
+        }
     }
 }
